edc9824de08af74c831323d74bb7655f3bc13ab4
nitomso <nitin.somanii@gmail.com>
2017-03-01 11:51:07 -0700

    Commiting It again

--- DBScripts/InsertScript.sql
+++ DBScripts/InsertScript.sql
@@ -1,3 +1,4 @@
 insert into pkhealthuser values( 1, 'rroy','rroy','Raja Roy','Admin');
 insert into pkhealthuser values( 2, 'snitin','snitin','Nitin Somani','Patient');
 insert into pkhealthuser values( 3, 'dilip','dilip','Dilip','patient');
+Testing Script
